5e5ba471ed8fde6b395fbfbe119944c960c84a1e
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-02-02 15:04:14 -0200

    Criação de mais scripts

---
+++ postgresql/curso/script_05b_fts.sql
@@ -0,0 +1,59 @@
+﻿-- criar coluna de busca:
+
+ALTER TABLE municipios ADD busca tsvector;
+
+-- popular coluna de busca:
+
+UPDATE municipios SET busca = to_tsvector(nome);
+
+-- efetuar busca usando FTS:
+
+SELECT nome, uf
+FROM municipios
+WHERE busca @@ plainto_tsquery('água');
+
+-- buscas com ranking:
+
+SELECT nome, uf, 
+	ts_rank_cd(
+		busca, 
+		to_tsquery('são & (carlos | pedro)'), 10) as rank
+FROM municipios
+WHERE busca @@ to_tsquery('são & (carlos | pedro)')
+ORDER BY rank DESC, nome, uf
+LIMIT 10;
+
+-- depuração dos parsers:
+
+SELECT ts_debug('15 contas no http://gmail.com');
+
+-- estatísticas do vetor de buscas:
+
+SELECT * 
+FROM ts_stat('SELECT busca FROM municipios')
+ORDER BY nentry DESC, ndoc DESC, word
+LIMIT 100;
+
+-- criar índice invertido:
+
+CREATE INDEX municipios_gidx ON municipios USING gin(busca);
+
+-- ***************************************
+
+-- Mantendo o índice atualizado:
+
+-- criar função de trigger:
+
+CREATE or replace FUNCTION municipios_trigger()
+RETURNS trigger AS $$
+	begin
+		new.busca := to_tsvector(new.nome);
+		return new;
+	end
+$$ LANGUAGE plpgsql;
+
+-- criar trigger de atualização:
+
+CREATE TRIGGER municipios_tsupdate
+BEFORE INSERT OR UPDATE ON municipios
+FOR EACH ROW EXECUTE PROCEDURE municipios_trigger();
---
+++ postgresql/curso/script_06_carga_em_massa.sql
@@ -0,0 +1,51 @@
+﻿-- exportando com COPY
+
+copy municipios to 'd:\temp\backup.txt';
+
+-- importando com COPY
+
+drop table if exists cidades;
+
+create table if not exists cidades as 
+select * 
+from municipios 
+where false;
+
+copy cidades from 'd:\temp\backup.txt';
+
+-- depois de importados, podemos criar as restrições e os índices.
+
+alter table cidades
+add constraint cidades_pkey
+primary key (id);
+
+ALTER TABLE cidades
+ADD CONSTRAINT cidades_estado_fk FOREIGN KEY (uf)
+REFERENCES estados (id);
+
+CREATE INDEX cidades_gidx
+ON cidades
+USING gin (busca);
+
+-- ******************************************************************
+
+-- Alguns outros exemplos variando um pouco a sintaxe:
+
+copy municipios to 'd:\temp\backup.bin'
+with (
+	freeze true,
+	FORMAT binary
+);
+
+copy municipios to 'd:\temp\backup.txt'
+with (
+	freeze true,
+	FORMAT text
+);
+
+copy municipios to 'd:\temp\backup.csv'
+with (
+	freeze true,
+	FORMAT csv,
+	delimiter '|'
+);
